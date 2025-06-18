7d52c39dfe5ade5d877ab573b9cb645b8b8b7a4d
CT5 <ct5@cherrytree56567.github.io>
2025-06-18 16:55:12 +1000

    Paging Test

--- AstralOS.Kernel/src/main.cpp
+++ AstralOS.Kernel/src/main.cpp
@@ -7,7 +7,7 @@ extern "C" int _start(BootInfo* pBootInfo) {
 
     memset(pBootInfo->pFramebuffer->BaseAddress, 0, pBootInfo->pFramebuffer->BufferSize);
     kernelServices.basicConsole.Println("Framebuffer Cleared");
-/*
+
     kernelServices.pageTableManager.MapMemory((void*)0x600000000, (void*)0x80000);
 	kernelServices.basicConsole.Println("Mapped 0x600000000 to 0x80000");
 
@@ -15,7 +15,6 @@ extern "C" int _start(BootInfo* pBootInfo) {
     *test = 26;
 
     kernelServices.basicConsole.Println(to_string(*test));
-*/
 
     while (true) {
         
--- Readme.md
+++ Readme.md
@@ -1,3 +1,26 @@
 # AstralOS
 
 A Simple AMD 64 UEFI EXT4 Operating System made in C and C++.
+
+## Building
+To build on Windows, install WSL (Windows Subsystem For Linux) and upgrade your distro to WSL 2.
+Then initialize cmake by running:
+```
+cmake .
+```
+To install Dependencies and setup directories run:
+```
+cmake --build build --target install
+```
+
+Then Build the Bootloader and Kernel by running:
+```
+cmake --build build --target build
+```
+
+## Features
+ - Paging
+
+## Thanks
+OSDev.org
+Poncho for the Poncho OS Paging Setup
