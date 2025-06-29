755fe2d26b33e010bc23029d96ec5c085b8225d0
Cherrytree56567 <124994670+Cherrytree56567@users.noreply.github.com>
2025-06-29 17:18:01 +1000

    Added ACPI - Part 3

--- AstralOS.Kernel/src/KernelServices/Paging/PageFrameAllocator/EFIMemoryMap/EFIMemoryMap.h
+++ AstralOS.Kernel/src/KernelServices/Paging/PageFrameAllocator/EFIMemoryMap/EFIMemoryMap.h
@@ -44,4 +44,5 @@ extern const char* EFI_MEMORY_TYPE_STRINGS[];
 
 uint64_t GetMemorySize(EFI_MEMORY_DESCRIPTOR* mMap, uint64_t mMapEntries, uint64_t mMapDescSize);
 
+extern "C" void memsetC(void* start, uint8_t value, uint64_t num);
 void memset(void* start, uint8_t value, uint64_t num);
