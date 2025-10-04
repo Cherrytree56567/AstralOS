15feb7ccab16e9dbf3c1460abfeb85c24d871bc0
CT5 <ct5@cherrytree56567.github.io>
2025-10-04 13:33:40 +1000

    Added EXT4 Driver - Part 1

---
+++ AstralOS.KernelDrivers/AstralOS.EXT4/src/GenericEXT4/GenericEXT4.cpp
@@ -0,0 +1,208 @@
+#include "GenericEXT4.h"
+#include "../global.h"
+#include <new>
+
+const char* to_hstridng(uint64_t value) {
+    static char buffer[19];
+    char* ptr = buffer + sizeof(buffer) - 1;
+    *ptr = '\0';
+
+    do {
+        uint8_t nibble = value & 0xF;
+        *--ptr = (nibble < 10) ? ('0' + nibble) : ('A' + nibble - 10);
+        value >>= 4;
+    } while (value > 0);
+
+    *--ptr = 'x';
+    *--ptr = '0';
+
+    return ptr;
+}
+
+bool GenericEXT4::Supports(const DeviceKey& devKey) {
+    if (devKey.bars[2] == 22) {
+        uint64_t dev = ((uint64_t)devKey.bars[0] << 32) | devKey.bars[1];
+        BaseDriver* bsdrv = (BaseDriver*)dev;
+        if (bsdrv->GetDriverType() == Partition) {
+            return true;
+        }
+    }
+    return false;
+}
+
+FilesystemDevice* GenericEXT4::CreateDevice() {
+    void* mem = g_ds->malloc(sizeof(GenericEXT4Device));
+    if (!mem) {
+        g_ds->Println("Failed to Malloc for Generic EXT4 Device");
+        return nullptr;
+    }
+
+    GenericEXT4Device* device = new(mem) GenericEXT4Device();
+    return device;
+}
+
+void* memset(void* dest, uint8_t value, size_t num) {
+    uint8_t* ptr = (uint8_t*)dest;
+    for (size_t i = 0; i < num; i++) {
+        ptr[i] = value;
+    }
+    return dest;
+}
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    unsigned char* d = (unsigned char*)dest;
+    const unsigned char* s = (const unsigned char*)src;
+
+    for (size_t i = 0; i < n; ++i)
+        d[i] = s[i];
+
+    return dest;
+}
+
+void GenericEXT4Device::Init(DriverServices& ds, DeviceKey& dKey) {
+    _ds = &ds;
+    devKey = dKey;
+
+    uint64_t dev = ((uint64_t)devKey.bars[0] << 32) | devKey.bars[1];
+    BaseDriver* bsdrv = (BaseDriver*)dev;
+    pdev = (PartitionDevice*)bsdrv;
+
+    _ds->Println("Initialized Generic EXT4 Driver");
+}
+
+/*
+ * 
+*/
+bool GenericEXT4Device::Support() {
+    _ds->Println(to_hstridng(pdev->SectorSize()));
+    if (pdev->SectorCount() == 0 || pdev->SectorSize() == 0) {
+        return false;
+    }
+    /*
+     * Here we can use LBA for LBA and LBASize
+    */
+    uint32_t LBA = 1024 / pdev->SectorSize();
+    uint32_t offset = 1024 % pdev->SectorSize();
+    if (LBA < 1) {
+        LBA = 1;
+    }
+
+    uint32_t LBASize = (1024 + pdev->SectorSize()) / pdev->SectorSize();
+
+    if (pdev->SectorCount() == 0) return false;
+
+    void* buf = _ds->RequestPage();
+    uint64_t bufPhys = (uint64_t)buf;
+    uint64_t bufVirt = bufPhys + 0xFFFFFFFF00000000;
+
+    _ds->Print(to_hstridng(bufVirt));
+
+    _ds->MapMemory((void*)bufVirt, (void*)bufPhys, false);
+    memset((void*)bufVirt, 0, 4096);
+
+    uint64_t offsetI = 0;
+    for (size_t i = 0; i < LBASize; i++) {
+        if (!pdev->ReadSector(LBA + i, (void*)(bufPhys + offsetI))) {
+            _ds->Println("Failed To Read Sector : EXT4");
+            return false;
+        }
+        offsetI += pdev->SectorSize();
+    }
+    superblock = (EXT4_Superblock*)(bufVirt + offset);
+
+    _ds->Print("FS Signature: ");
+    _ds->Println(to_hstridng(superblock->MagicSignature));
+
+    if (superblock->MagicSignature == 0xEF53) {
+        return true;
+    }
+    return false;
+}
+
+FsNode* GenericEXT4Device::Mount() {
+    if (!Support()) {
+        return NULL;
+    }
+}
+
+bool GenericEXT4Device::Unmount() {
+    
+}
+
+FsNode* GenericEXT4Device::ListDir(FsNode* node) {
+    
+}
+
+FsNode* GenericEXT4Device::FindDir(FsNode* node, const char* name) {
+    
+}
+
+FsNode* GenericEXT4Device::CreateDir(FsNode* parent, const char* name) {
+    
+}
+
+bool GenericEXT4Device::Remove(FsNode* node) {
+    
+}
+
+File* GenericEXT4Device::Open(FsNode* node, uint32_t flags) {
+    
+}
+
+bool GenericEXT4Device::Close(File* file) {
+    
+}
+
+int64_t GenericEXT4Device::Read(File* file, void* buffer, uint64_t size) {
+    
+}
+
+int64_t GenericEXT4Device::Write(File* file, void* buffer, uint64_t size) {
+    
+}
+
+bool GenericEXT4Device::Stat(FsNode* node, FsNode* out) {
+    
+}
+
+bool GenericEXT4Device::Chmod(FsNode* node, uint32_t mode) {
+    
+}
+
+bool GenericEXT4Device::Chown(FsNode* node, uint32_t uid, uint32_t gid) {
+    
+}
+
+bool GenericEXT4Device::Utimes(FsNode* node, uint64_t atime, uint64_t mtime, uint64_t ctime) {
+    
+}
+
+uint8_t GenericEXT4Device::GetClass() {
+    return 0x0;
+}
+
+uint8_t GenericEXT4Device::GetSubClass() {
+    return 0x0;
+}
+
+uint8_t GenericEXT4Device::GetProgIF() {
+    return 0x0;
+}
+
+const char* GenericEXT4Device::name() const {
+    return "";
+}
+
+/*
+ * Make sure you return somthing
+ * otherwise you will get weird
+ * errors, like code that ends up
+ * at 0x8 in mem.
+*/
+const char* GenericEXT4Device::DriverName() const {
+    return _ds->strdup("Generic EXT4 Driver");
+}
+
+PartitionDevice* GenericEXT4Device::GetParentLayer() {
+    return pdev;
+}
